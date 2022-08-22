433bdb746fd68192463f61478159cd3726678026
!nwgodrip <73049524+danunai1993@users.noreply.github.com>
2022-08-23 01:27:25 +0700

    Update tester.py

--- tester.py
+++ tester.py
@@ -2,16 +2,17 @@ import numpy as np
 import cv2 
 
 img = cv2.resize(cv2.imread("screenshot.png",0), (0, 0), fx=0.4, fy=0.4)
-template = cv2.resize(cv2.imread("addaccount.png",0), (0, 0), fx=0.8, fy=0.8)
+template = cv2.resize(cv2.imread("listgmail.png",0), (0, 0), fx=0.8, fy=0.8)
 
-"""
+""" method ที่ตรวจจับเจอรูปภาพ
+gmail.png       TM_CCOEFF,TM_CCOEFF_NORMED,TM_CCORR_NORMED,TM_SQDIFF,TM_SQDIFF_NORMED
+listgmail.png   TM_CCOEFF_NORMED,TM_CCORR_NORMED,TM_SQDIFF,TM_SQDIFF_NORMED
 addaccount.png  TM_CCOEFF,TM_CCOEFF_NORMED,TM_CCORR_NORMED,TM_SQDIFF,TM_SQDIFF_NORMED
 google2.png     TM_CCOEFF_NORMED,TM_SQDIFF,TM_SQDIFF_NORMED
 
 """
 
 h, w = template.shape
-
 methods = [ cv2.TM_CCOEFF, cv2.TM_CCOEFF_NORMED, cv2.TM_CCORR,
             cv2.TM_CCORR_NORMED, cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]
 
@@ -25,7 +26,8 @@ for method in methods:
         location = max_loc
 
     bottom_right = (location[0] + w , location[1] + h)
-    cv2.rectangle(img2, location, bottom_right, (0,0,255), 5)
+    color = (255, 0, 0)
+    cv2.rectangle(img2, location, bottom_right, (0,0,255), 3)
     cv2.imshow('Match', img2)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
